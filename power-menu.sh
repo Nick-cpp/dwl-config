2bbd0c8f1fb7f6b463726833092745df1d1a776f
Mikola 2.0 <gnots.nik@gmail.com>
2026-09-02 22:09:50 +0500

    Update power-menu.sh to use dwl instead of Hyprland

--- power-menu.sh
+++ power-menu.sh
@@ -4,16 +4,16 @@ choice=$(echo -e "Reboot\nShutdown" | wmenu -f "JetBrainsMono Nerd Font Bold 12"
 
 case "$choice" in
     "Reboot")
-        confirm=$(echo -e "Yes\nNo" | wmenu -p "Reboot?")
+        confirm=$(echo -e "Yes\nNo" | wmenu -f "JetBrainsMono Nerd Font Bold 12" -p "Reboot?")
         if [ "$confirm" = "Yes" ]; then
-            pkill -15 -x Hyprland
+            pkill -15 -x dwl
             loginctl reboot
         fi
         ;;
     "Shutdown")
-        confirm=$(echo -e "Yes\nNo" | wmenu -p "Shutdown?")
+        confirm=$(echo -e "Yes\nNo" | wmenu -f "JetBrainsMono Nerd Font Bold 12" -p "Shutdown?")
         if [ "$confirm" = "Yes" ]; then
-            pkill -15 -x Hyprland
+            pkill -15 -x dwl
             loginctl poweroff
         fi
         ;;
