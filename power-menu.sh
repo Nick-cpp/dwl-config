fd3d225cdc77bb39f35af2a86c0e706b3e26d428
Mikola 2.0 <gnots.nik@gmail.com>
2026-09-07 21:17:56 +0500

    Refactor wmenu options for power menu script

--- power-menu.sh
+++ power-menu.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
-choice=$(echo -e "Reboot\nShutdown" | wmenu -f "JetBrainsMono Nerd Font Bold 12" -p "Power Menu:")
+WMENU_OPTS="-f 'JetBrainsMono Nerd Font Bold 12' -N '#1e2030' -n '#c0caf5' -S '#7aa2f7' -s '#15161e' -M '#1e2030'"
+
+choice=$(echo -e "Reboot\nShutdown" | eval wmenu $WMENU_OPTS -p '"Power Menu:"')
 
 case "$choice" in
     "Reboot")
-        confirm=$(echo -e "Yes\nNo" | wmenu -f "JetBrainsMono Nerd Font Bold 12" -p "Reboot?")
+        confirm=$(echo -e "Yes\nNo" | eval wmenu $WMENU_OPTS -p '"Reboot?"')
         if [ "$confirm" = "Yes" ]; then
             pkill -15 -x dwl
             loginctl reboot
         fi
         ;;
     "Shutdown")
-        confirm=$(echo -e "Yes\nNo" | wmenu -f "JetBrainsMono Nerd Font Bold 12" -p "Shutdown?")
+        confirm=$(echo -e "Yes\nNo" | eval wmenu $WMENU_OPTS -p '"Shutdown?"')
         if [ "$confirm" = "Yes" ]; then
             pkill -15 -x dwl
             loginctl poweroff
